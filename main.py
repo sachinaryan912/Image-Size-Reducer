f574d1576478f389dae3cba8dbc2a4710dc1e256
sachinaryan912 <67205331+sachinaryan912@users.noreply.github.com>
2022-10-26 15:38:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PIL import Image
 # load the image
-image = Image.open('opera_house.jpg')
+image = Image.open('sample.jpg')
 # report the size of the image
 print(image.size)
 print(image.format)
@@ -8,7 +8,7 @@ print(image.format)
 img_resized = image.resize((200,200))
 # report the size of the thumbnail
 print(img_resized.size)
-img_resized.save('opera_house3.jpg', format='JPEG')
+img_resized.save('sample2.jpg', format='JPEG')
 # load the image again and inspect the format
-image2 = Image.open('opera_house2.png')
+image2 = Image.open('sample2.jpg')
 print(image2.format)
